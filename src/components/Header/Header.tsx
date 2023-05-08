b169fc236b4e681187fc27a84c41aca0b0bcbb34
Egor Martynov <egmaegma02@gmail.com>
2023-05-08 23:12:21 +0500

    Initial commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styles from '../../App.css'
+import logoIcon from './logo.png'
+import headerStyles from './Header.module.css'
+const Header = () => {
+    return (
+        <div className={styles.container}>
+            <header className={headerStyles.wrapper}>
+                <div className={headerStyles.header}>
+                    <img src={logoIcon} className={headerStyles.logo} alt=""/>
+                    <p className={headerStyles.logoTextFirst}>Hoo</p>
+                    <p className={headerStyles.logoTextSecond}>Bank</p>
+                    <div className={headerStyles.menu}>
+                        <ul>
+                            <li>Home</li>
+                            <li>About us</li>
+                            <li>Features</li>
+                            <li>Solution</li>
+                        </ul>
+                    </div>
+                </div>
+
+
+            </header>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,107 @@
+import React from 'react';
+import banner from './assets/banner.png'
+import card1 from './assets/1slide.png'
+import card2 from './assets/2slide.png'
+import card3 from './assets/3slide.png'
+import robotHand from './assets/robot-hand-background-presenting-technology-gesture 1.png'
+import sphere from './assets/Vector.png'
+import mainStyles from './Main.module.css'
+import styles from '../../App.css'
+import {motion} from "framer-motion"
+
+const Main = () => {
+    return (
+        <div className={styles.container}>
+            <div className={mainStyles.wrapper}>
+                <div className={mainStyles.hero}>
+                    <div className={mainStyles.mainLeft}>
+                        <motion.img
+                            initial={{y: -500}}
+                            animate={{y: 0}}
+                            transition={{ease: 'easeOut', duration: 1}}
+
+                            src={banner} alt=""
+                        />
+                        <motion.p
+                            initial={{x: -500}}
+                            animate={{x: 0}}
+                            transition={{ease: 'easeOut', duration: 1}}
+                            className={mainStyles.mainText}>The Next <span>Generation</span> Payment Method.
+                        </motion.p>
+                        <motion.p
+                            initial={{x: -500}}
+                            animate={{x: 0}}
+                            transition={{ease: 'easeOut', duration: 1.5}}
+
+                            className={mainStyles.desc}>Our team of experts uses a methodology to identify the credit
+                            cards most likely to fit your needs.
+                            We examine annual percentage rates, annual fees.
+                        </motion.p>
+                    </div>
+                    <div className={mainStyles.mainRight}>
+                        <motion.img
+                            initial={{scale: 0}}
+                            animate={{scale: 1}}
+                            transition={{ease: 'easeOut', duration: 2}}
+                            src={sphere} className={mainStyles.sphereTop} alt=""
+                        />
+                        <motion.img
+                            initial={{scale: 0}}
+                            animate={{scale: 1}}
+                            transition={{ease: 'easeOut', duration: 1}}
+                            src={sphere} className={mainStyles.sphereBot} alt=""
+                        />
+                        <motion.img
+                            initial={{scale: 0}}
+                            animate={{scale: 1}}
+                            transition={{ease: 'easeOut', duration: 1.5}}
+                            src={sphere} className={mainStyles.sphereRght} alt=""
+                        />
+
+                        <div className={mainStyles.cardsGradient}>
+                            <div className={mainStyles.cards}>
+                                <motion.img
+                                    initial={{y: -100}}
+                                    animate={{y: 0,}}
+                                    transition={{ease: 'easeOut', duration: 2.3}}
+
+
+                                    src={card3} className={mainStyles.card3} alt=""
+                                />
+                                <motion.img
+                                    initial={{y: -200}}
+                                    animate={{y: 0}}
+                                    transition={{ease: 'easeOut', duration: 2}}
+
+                                    src={card2} className={mainStyles.card2} alt=""
+                                />
+                                <motion.img
+                                    initial={{y: -300}}
+                                    animate={{y: 0}}
+                                    transition={{ease: 'easeOut', duration: 1.7}}
+
+
+                                    src={card1} className={mainStyles.card1} alt=""
+                                />
+                            </div>
+                        </div>
+                        <motion.img
+                            initial={{x: 500}}
+                            animate={{x: 0}}
+                            transition={{ease: "easeOut", duration: 1.5}}
+                            whileHover={{
+                                scale: 1.1,
+                                transition: { duration: 0.5 },
+                            }}
+
+                            src={robotHand} alt="" className={mainStyles.robotHand}/>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    );
+};
+
+export default Main;
